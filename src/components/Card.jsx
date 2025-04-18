3603e3543d3e9c0321cb670069a6f94de2e00e06
Pranav Nair <pp6275@srmist.edu.in>
2025-04-18 21:28:13 +0530

    Wiping old and obsolete files

--- src/components/Card.jsx
+++
@@ -1,6 +0,0 @@
-// 'children' is the reserved prop name for any content passed in openin and closin tags of a component
-const Card = ({ children, bg = "bg-gray-100" }) => {
-  return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>;
-};
-
-export default Card;
